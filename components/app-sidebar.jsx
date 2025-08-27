21eeec902c53f315af7151e59ec0c66fe3f14a19
BothSann <boththann76@gmail.com>
2025-08-28 00:11:52 +0700

    Add sidebar components: Implement AppSidebar, NavMain, NavProjects, NavUser, TeamSwitcher, and UI elements including Avatar, Breadcrumb, Checkbox, Collapsible, Input, Label, Select, Separator, Skeleton, Table, Textarea, Tooltip, and Sidebar. Enhance layout and functionality for improved user experience.

---
+++ components/app-sidebar.jsx
@@ -0,0 +1,71 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Logo from "./Logo";
+import Link from "next/link";
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "/admin/dashboard",
+    icon: Home,
+  },
+  {
+    title: "Products",
+    url: "/admin/products",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup className="px-4 py-6">
+          <SidebarGroupLabel>
+            <Logo />
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu className="gap-4 mt-6">
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
