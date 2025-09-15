43eb0a011cf379350d8e67c397301f0d469052fe
BothSann <boththann76@gmail.com>
2025-09-15 19:46:32 +0700

    Refactor ProductTable component to enhance readability and maintainability: Introduce a dynamic table header configuration and integrate pagination for improved user navigation. Update table structure for better alignment with data representation.

--- components/ProductTable.js
+++ components/ProductTable.js
@@ -4,13 +4,28 @@ import {
   TableRow,
   TableHead,
   TableBody,
+  TableFooter,
+  TableCell,
 } from "@/components/ui/table";
 import ProductRow from "./ProductRow";
+import Pagination from "./Pagination";
+
 import { getAllProducts } from "@/lib/apiProducts";
+import { cn } from "@/lib/utils";
 
 export default async function ProductTable() {
   const { products } = await getAllProducts();
 
+  const tableHeaders = [
+    { label: "Product Name", key: "name" },
+    { label: "Base Price", key: "base_price" },
+    { label: "Discount", key: "discount_percentage" },
+    { label: "Category", key: "category_name" },
+    { label: "Stock", key: "total_stock" },
+    { label: "Code", key: "product_code" },
+    { label: "Status", key: "is_active" },
+    { label: "Actions", key: "actions" },
+  ];
   // console.log(products);
 
   // Simulate a delay
@@ -20,16 +35,16 @@ export default async function ProductTable() {
 
   return (
     <Table className="mt-10 border rounded-lg">
-      <TableHeader className="text-base">
+      <TableHeader>
         <TableRow>
-          <TableHead>Product Name</TableHead>
-          <TableHead>Base Price</TableHead>
-          <TableHead>Discount</TableHead>
-          <TableHead>Category</TableHead>
-          <TableHead>Stock</TableHead>
-          <TableHead>Code</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Actions</TableHead>
+          {tableHeaders.map((header, index) => (
+            <TableHead
+              className={cn("py-3.5", index === 0 && "pl-4")}
+              key={header.key}
+            >
+              {header.label}
+            </TableHead>
+          ))}
         </TableRow>
       </TableHeader>
       <TableBody>
@@ -37,6 +52,10 @@ export default async function ProductTable() {
           <ProductRow key={product.id} product={product} />
         ))}
       </TableBody>
+
+      <TableFooter>
+        <Pagination />
+      </TableFooter>
     </Table>
   );
 }
