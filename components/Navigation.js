3547199672aef9b94ca5c690c7590536d4c568a0
Both Sann <thannsopheakboth@gmail.com>
2025-10-12 19:01:16 +0700

    Refactor Navigation component: Remove useAuthorization hook and loading state handling, simplifying the rendering logic. Ensure the dashboard link is always displayed while maintaining other navigation elements.

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,31 +2,24 @@
 
 import Link from "next/link";
 import CustomerProfileDropdownMenu from "@/components/customer/CustomerProfileDropdownMenu";
-import useAuthorization from "@/hooks/useAuthorization";
+
 import { Heart } from "lucide-react";
 import { ModeToggle } from "@/components/ModeToggle";
 import { Button } from "./ui/button";
 import CartDrawer from "@/components/CartDrawer";
 
 export default function Navigation() {
-  const { isAuthenticated, isLoading } = useAuthorization();
-
-  if (isLoading) {
-    return null;
-  }
-
   return (
     <nav className="z-10">
       <ul className="flex items-center gap-2">
-        {isAuthenticated() && (
-          <>
-            <Button variant="ghost" asChild>
-              <Link href="/dashboard">
-                <Heart className="scale-125" />
-              </Link>
-            </Button>
-          </>
-        )}
+        <>
+          <Button variant="ghost" asChild>
+            <Link href="/dashboard">
+              <Heart className="scale-125" />
+            </Link>
+          </Button>
+        </>
+
         <CartDrawer />
         <CustomerProfileDropdownMenu />
         <ModeToggle />
