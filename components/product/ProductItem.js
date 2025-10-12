fc138c69c3dbcbce24e4ef6ec5b678acf70bcfb2
Both Sann <thannsopheakboth@gmail.com>
2025-10-12 13:56:05 +0700

    Enhance product display with discount handling: Update ProductCustomerDetail and ProductItem components to show discounted prices and badges for products with discounts. Improve layout and styling for better user experience.

--- components/product/ProductItem.js
+++ components/product/ProductItem.js
@@ -1,38 +1,51 @@
 import { formatCurrency } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { Badge } from "../ui/badge";
 
 export default function ProductItem({ product }) {
+  const discountedPrice = product.discounted_price;
+  const discountPercentage = product.discount_percentage;
+  const hasDiscount = product.discount_percentage > 0;
+
   return (
     <li key={product.id}>
       <Link href={`/products/${product.id}`}>
-        <ProductImage product={product} />
-        <ProductInfo product={product} />
+        {/* Product Image */}
+        <div className="relative aspect-[3/4]">
+          <Image
+            alt={product.name}
+            src={product.banner_image_url}
+            fill
+            className="object-cover object-center"
+          />
+
+          {hasDiscount && (
+            <Badge
+              variant="destructive"
+              className="absolute top-0 right-0 z-10 text-base"
+            >
+              {discountPercentage}% off
+            </Badge>
+          )}
+        </div>
+
+        {/* Product Info */}
+        <div className="py-4 px-2 flex flex-col gap-0.5">
+          <p className="text-lg">{product.name}</p>
+          {hasDiscount ? (
+            <p className="font-jost text-primary/80">
+              {`from ${formatCurrency(product.base_price)} to ${formatCurrency(
+                discountedPrice
+              )}`}
+            </p>
+          ) : (
+            <p className="font-jost text-primary/80">
+              {formatCurrency(product.base_price)}
+            </p>
+          )}
+        </div>
       </Link>
     </li>
-  );
-}
-
-function ProductImage({ product }) {
-  return (
-    <div className="relative aspect-[3/4]">
-      <Image
-        alt={product.name}
-        src={product.banner_image_url}
-        fill
-        className="object-cover object-center"
-      />
-    </div>
-  );
-}
-
-function ProductInfo({ product }) {
-  return (
-    <div className="py-4 px-2 flex flex-col gap-0.5">
-      <p className="text-lg">{product.name}</p>
-      <p className=" font-jost text-primary/80">
-        from {formatCurrency(product.base_price)}
-      </p>
-    </div>
   );
 }
