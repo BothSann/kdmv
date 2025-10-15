8a4108c05e47f713c84ade2b5c71733036f3cfa2
Both Sann <thannsopheakboth@gmail.com>
2025-10-16 00:43:50 +0700

    Enhance layout components by adding full-height styling to CustomerLayout and adjusting overflow properties in AccountLayout for better responsiveness and user experience.

--- app/(customer)/layout.js
+++ app/(customer)/layout.js
@@ -2,7 +2,7 @@ import Header from "@/components/Header";
 
 export default function CustomerLayout({ children }) {
   return (
-    <div className="font-poppins">
+    <div className="h-screen font-poppins">
       <Header />
       <main className="max-w-7xl mx-auto w-full p-8 px-6 lg:px-8">
         {children}
