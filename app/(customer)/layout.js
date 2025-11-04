0055c4f60a3ad7009b46fcf518291d755d0ae22f
Both Sann <thannsopheakboth@gmail.com>
2025-11-04 16:31:55 +0700

    Update .gitignore to include new documentation files and enhance CustomerLayout component with full height styling for improved layout consistency.

--- app/(customer)/layout.js
+++ app/(customer)/layout.js
@@ -3,9 +3,9 @@ import Header from "@/components/Header";
 
 export default function CustomerLayout({ children }) {
   return (
-    <div className="font-poppins">
+    <div className="font-poppins h-screen">
       <Header />
-      <main className="max-w-7xl mx-auto w-full py-12 lg:py-16 px-6 lg:px-0">
+      <main className="max-w-7xl mx-auto w-full py-12 lg:py-20 px-6 lg:px-0 h-full">
         {children}
       </main>
       <ConditionalFooter />
