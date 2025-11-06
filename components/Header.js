f4364c6960722f03830b41ce80b2befa1a29f77c
Both Sann <thannsopheakboth@gmail.com>
2025-11-06 18:03:13 +0700

    Update logo sizing in Header and HeaderWithCarousel components for consistency across different screen sizes

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="px-8 py-5 border-b border-border">
       <nav className="flex items-center justify-between max-w-7xl mx-auto">
-        <Logo width="w-24 lg:w-28" />
+        <Logo width="w-20 md:w-24 lg:w-28" />
         <Navigation />
       </nav>
     </header>
