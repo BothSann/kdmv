24a8ea30520040a316733433a93d26c5d8e0a15f
Both Sann <thannsopheakboth@gmail.com>
2025-11-10 16:48:44 +0700

    Add DisclaimerBanner component and integrate into IndexLayout
    - Introduced a new DisclaimerBanner component to display a warning about the official KDMV website.
    - Integrated the DisclaimerBanner into the IndexLayout for improved user awareness.

--- app/(index)/layout.js
+++ app/(index)/layout.js
@@ -1,9 +1,13 @@
 import ConditionalFooter from "@/components/ConditionalFooter";
+import DisclaimerBanner from "@/components/DisclaimerBanner";
 import HeaderWithCarousel from "@/components/HeaderWithCarousel";
 
 export default function IndexLayout({ children }) {
   return (
     <div>
+      {/* Warning Banner */}
+      <DisclaimerBanner />
+
       <HeaderWithCarousel />
       <main className="max-w-7xl mx-auto w-full px-6 lg:px-8 py-6 lg:py-12">
         {children}
---
+++ components/DisclaimerBanner.js
@@ -0,0 +1,30 @@
+import { Megaphone, MoveRight } from "lucide-react";
+import Link from "next/link";
+
+export default function DisclaimerBanner() {
+  return (
+    <div className="h-auto w-full bg-warning-bg text-warning-text lg:py-4 py-2.5 lg:px-6 px-4.5 text-center">
+      <div className="flex items-center justify-center gap-2">
+        <span className="text-xs lg:text-sm text-center">
+          <Megaphone className="size-4 inline-block mr-1" />
+          <span>
+            This is not the official KDMV website. Please visit the official
+            website at{" "}
+          </span>
+          <Link
+            href="https://www.kdmv.io"
+            className="group font-bold text-warning-text transition-all duration-300 inline-flex items-center gap-1 px-1 hover:underline hover:scale-105"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>kdmv.io</span>
+            <MoveRight
+              size={18}
+              className="opacity-0 group-hover:opacity-100 transition-all duration-300"
+            />
+          </Link>
+        </span>
+      </div>
+    </div>
+  );
+}
